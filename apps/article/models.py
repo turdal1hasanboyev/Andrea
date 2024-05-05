8472aca755c645f521f6d7ee877ec22d36f93440
turdal1hasanboyev <turdalihasanboyev70.com>
2024-05-05 21:03:43 +0500

    Write Model

--- apps/article/models.py
+++ apps/article/models.py
@@ -18,14 +18,14 @@ class Article(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to="Avatars/")
+    image = models.ImageField(upload_to="Article/")
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag,blank=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.title
     
-class Coment(models.Model):    
+class Comment(models.Model):    
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     user = models.ForeignKey(Account,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=225)
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
-urlpatterns = [ 
-    
+urlpatterns = [
+    path('register/', views.register_view, name="register"),
+    path('logout/', views.logout_view, name="logout"),
+    path('login/', views.login_view, name="login")
 ]
