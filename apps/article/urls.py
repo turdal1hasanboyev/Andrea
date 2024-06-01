2fb443e76377caea7bb8d5c217cdff27ccf899ad
turdal1hasanboyev <turdalihasanboyev70.com>
2024-06-01 19:09:33 +0500

    Finish Project

--- apps/article/urls.py
+++ apps/article/urls.py
@@ -4,7 +4,7 @@ from .views import index, single, travel, fashion, about
 
 urlpatterns = [
     path('', index, name="index"),
-    path('detail/<slug:slug>/', single, name='single'),
+    path('single/<slug:slug>/', single, name='single'),
     path('travel/', travel, name='travel'),
     path('fashion/', fashion, name='fashion'),
     path('about/', about, name='about'),
