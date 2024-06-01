2536a9e7aa04a0a7d9f8b1bcb28c69c3779f74eb
Xondamir0616 <98737174+Xondamir0616@users.noreply.github.com>
2024-06-01 17:44:01 +0500

    Update views.py

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 
-
 def get_in_touch(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect("contact")
+        return redirect('contact_success')
     
-    return render(request, "contact.html", {"form": form})
+    return render(request, 'contact.html', {'form': form})
